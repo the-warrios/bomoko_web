8c09c5b5e7c9a5bf62abf6607abcc7bdef383ef9
Oslo1308 <oslovie.nsole@wolftech.online>
2024-10-15 10:21:16 +0100

    update login

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -49,7 +49,7 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('/admin'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
